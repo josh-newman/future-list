ba8e004bc68c6eacef7255b3cbf174e029f27b21
Josh Newman <josh@josh-newman.com>
2014-08-29 02:51:33 -0700

    add extract test

--- src/test/scala/futurelist/FutureListTest.scala
+++ src/test/scala/futurelist/FutureListTest.scala
@@ -1,16 +1,25 @@
 package futurelist
 
 import org.junit.Test
+import org.scalatest.junit.AssertionsForJUnit
 
-import scala.concurrent.Future
-
-class FutureListTest {
+class FutureListTest extends AssertionsForJUnit {
 
   import futurelist._
 
   @Test
   def construct(): Unit = {
     val futureList = 1 !:: 2 !:: FutureNil
+  }
+
+  @Test
+  def extract(): Unit = {
+    val (head, _) = 1 !:: FutureNil match {
+      case h !:: t => (h, t)
+      case other => fail(s"Should have matched: $other")
+    }
+
+    assert(1 === head)
   }
 
 }
